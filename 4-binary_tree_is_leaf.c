0328ee716d6667b8e7aafb4d08606471dc981002
ifiok1equere <equereifiok1@gmail.com>
2023-08-29 15:46:43 +0100

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - functon checks if a node is a leaf
+ * @node: pointer to node to be checked
+ *
+ * Return: 1 for leaf, 0 otherwise/NULL
+ */
+
+binary_tree_is_leaf(const binary_tree_t *node);
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
+}
