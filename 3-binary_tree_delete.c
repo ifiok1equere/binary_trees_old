9079ed99ec36a9527ea8a8bb467666a12a9d5224
ifiok1equere <equereifiok1@gmail.com>
2023-08-29 15:26:31 +0100

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function deletes an entire tree
+ * @tree: pointer to the entire tree
+ *
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->parent);
+
+	free(tree);
+}
