2e06a3fda15ee72dc60c982d1f8941c9ee5a7585
ifiok1equere <equereifiok1@gmail.com>
2023-08-29 22:05:02 +0100

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,12 +13,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-	left_count = binary_tree_height(tree->left);
-	right_count = binary_tree_height(tree->right);
-
-	if (left_count >= right_count)
-		return (left_count + 1);
 	else
-		return (right_count + 1);
+	{
+		if (tree->left)
+			left_count = 1 + binary_tree_height(tree->left);
+		else
+			left_count = 0;
+		if (tree->right)
+			right_count = 1 + binary_tree_height(tree->right);
+		else
+			right_count = 0;
+
+		if (left_count >= right_count)
+			return (left_count);
+		else
+			return (right_count);
+	}
 }
