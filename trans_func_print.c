354220cb1422dfafa4df8c0441391c4685850184
ifiok1equere <equereifiok1@gmail.com>
2023-08-29 16:14:00 +0100

    node data print function when transversing a tree

---
+++ trans_func_print.c
@@ -0,0 +1,11 @@
+#include "binary_tress.h"
+
+/**
+ * func - function to print a node data
+ * @data: node data to be printed
+ *
+ * Return: void
+ */
+
+void func(int data)
+	printf("%d\n", data);
