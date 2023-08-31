52669c0bdeb56bb2f6d50f3b2987706dff404d4d
ifiok1equere <equereifiok1@gmail.com>
2023-08-31 09:32:20 +0100

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,7 +11,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t left_count, right_count;
 
-	if (tree == NULL || tree->parent == NULL)
+	if (tree == NULL)
 		return (0);
 
 	left_count = binary_tree_nodes(tree->left);
@@ -19,6 +19,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree->left != NULL || tree->right != NULL)
 		return (left_count + right_count + 1);
-	else
-		return (left_count + right_count);
+	return (left_count + right_count);
 }
