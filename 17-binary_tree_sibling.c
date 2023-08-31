ceb3a31088e020f90a5ea1995f971a8fad0f5eae
ifiok1equere <equereifiok1@gmail.com>
2023-08-31 14:38:04 +0100

    task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - function returns the sibling of a node in a binary tree
+ * @node: node whose sibling is to be found.
+ *
+ * Return: pointer to node's sibling
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+
+	if (node->parent->left != node)
+		return (node->parent->left);
+	else
+		return (node->parent->right);
+}
