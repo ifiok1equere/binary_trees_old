6388371fea9761bceed5b90275179484079a8b61
ifiok1equere <equereifiok1@gmail.com>
2023-08-31 14:03:11 +0100

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle -  function returns the uncle of a node in a binary tree
+ * @node: node whose uncle is to be found.
+ *
+ * Return: pointer to node's uncle
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent->parent == NULL)
+		return (NULL);
+
+	/*printf("node: %d\n", node->n);
+	printf("node->parent->parent-left: %d\n", node->parent->parent->left->n);*/
+	if (node->parent->parent->left != node->parent)
+		return (node->parent->parent->left);
+	else
+		return (node->parent->parent->right);
+}
