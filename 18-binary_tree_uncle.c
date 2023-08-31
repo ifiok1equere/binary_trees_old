7543909a58034b9331aa056d5eab6a4ef0a27f84
ifiok1equere <equereifiok1@gmail.com>
2023-08-31 14:11:31 +0100

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -15,6 +15,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent->parent == NULL)
 		return (NULL);
 
+	if (node->parent->parent->left == NULL || node->parent->parent->right == NULL)
+		return (NULL);
+
 	if (node->parent->parent->left != node->parent)
 		return (node->parent->parent->left);
 	else
